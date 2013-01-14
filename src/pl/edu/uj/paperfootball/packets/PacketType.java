97fdf07d5cad083087afbb0b837a4d9a65f220bc
AdamMarkowski <szalonymjut@gmail.com>
2013-01-14 15:01:43 +0100

    usunięcie bb

--- src/pl/edu/uj/paperfootball/packets/PacketType.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.uj.paperfootball.packets;
-
-/**
- * Enumeration representing available types of packets that can be send via Bluetooth.
- */
-public enum PacketType {
-	NEW_MOVE, REQUEST_GAME_LOAD, CONTINUE_GAME, REQUEST_SHOW_GAME_REPLAY, ACCEPT_OR_REJECT, REQUEST_REPEAT_MOVE, REQUEST_CANCEL, PACKET_NO_TYPE
-}
